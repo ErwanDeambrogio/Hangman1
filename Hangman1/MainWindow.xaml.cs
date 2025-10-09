0d6699215c1688018a3c9be26f702fb55893c833
SLAB43 <SLAB43@KM102-09-M>
2025-10-09 11:59:10 +0200

    .

--- Hangman1/MainWindow.xaml.cs
+++ Hangman1/MainWindow.xaml.cs
@@ -13,16 +13,48 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Hangman
+
+namespace Hangman1
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
+
+
+
     public partial class MainWindow : Window
     {
+
+        // variable global
+        Random rand = new Random();
+        string[] list_mots = { "ordinateur", "souris", "clavier", "ecran", "telephone" };
+        string selectedword;
+        int vie = 0;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        public void StartGame()
+        {
+            string mot = list_mots[rand.Next(list_mots.Length)];
+            char[] motAffiche = new char[mot.Length];
+            for (int i = 0; i < mot.Length; i++)
+            {
+                motAffiche[i] = '*';
+            }
+
+            vie = 5;
+
+        }
+
+        private void BTN_Letter_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;    
+            if (btn != null)
+            {
+                string lettre = btn.Content.ToString(); 
+
+            }
+        }
     }
 }
+
