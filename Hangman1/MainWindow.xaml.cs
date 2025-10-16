bd5c73bf148dedd81f47c765471698d89ae0b7ce
SLAB43 <SLAB43@KM102-09-M>
2025-10-16 11:56:12 +0200

    .

--- Hangman1/MainWindow.xaml.cs
+++ Hangman1/MainWindow.xaml.cs
@@ -1,89 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Resources;
-using System.Windows.Shapes;
-
 
 namespace Hangman1
 {
+    public partial class MainWindow : Window
+    {
+        private string[] list_mots = { "chat", "chien", "maison", "ordinateur", "voiture" };
+        private string motMystere;
+        private char[] motAffiche;
+        private int vie = 5;
 
-
-
-        public partial class MainWindow : Window
+        public MainWindow()
         {
-            private string[] list_mots = { "chat", "chien", "maison", "ordinateur", "voiture" };
-            private string motMystere;
-            private char[] motAffiche;
-            int vie = 5;
+            InitializeComponent();
+            InitialiserJeu();
+        }
 
-            public MainWindow()
+        private void InitialiserJeu()
+        {
+            Random rand = new Random();
+            motMystere = list_mots[rand.Next(list_mots.Length)];
+
+            motAffiche = new char[motMystere.Length];
+            for (int i = 0; i < motMystere.Length; i++)
             {
-                InitializeComponent();
-                InitialiserJeu();
+                motAffiche[i] = '_';
             }
 
-            private void InitialiserJeu()
+            Txt_MotCache.Text = string.Join(" ", motAffiche);
+
+            vie = 5;
+            Lbl_Vies.Content = "Vies : " + vie;
+
+            ReinitialiserBoutons();
+        }
+
+        private void BTN_Letter_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null) return;
+
+            char lettre = btn.Content.ToString()[0];
+            btn.IsEnabled = false;
+
+            bool trouve = false;
+
+            for (int i = 0; i < motMystere.Length; i++)
             {
-                Random rand = new Random();
-                motMystere = list_mots[rand.Next(list_mots.Length)];
-
-                motAffiche = new char[motMystere.Length];
-                for (int i = 0; i < motMystere.Length; i++)
+                if (char.ToLower(motMystere[i]) == char.ToLower(lettre))
                 {
-                    motAffiche[i] = '_';
+                    motAffiche[i] = motMystere[i];
+                    trouve = true;
                 }
-
-                Txt_MotCache.Text = string.Join(" ", motAffiche);
             }
 
-            private void BTN_Letter_Click(object sender, RoutedEventArgs e)
+            Txt_MotCache.Text = string.Join(" ", motAffiche);
+
+            if (!trouve)
             {
-                Button btn = sender as Button;
-                if (btn == null) return;
-
-                char lettre = btn.Content.ToString()[0];
-                btn.IsEnabled = false;
-
-                bool trouve = false;
-
-                for (int i = 0; i < motMystere.Length; i++)
-                {
-                    if (char.ToLower(motMystere[i]) == char.ToLower(lettre))
-                    {
-                        motAffiche[i] = motMystere[i];
-                        trouve = true;
-                    }
-                }
-
-                Txt_MotCache.Text = string.Join(" ", motAffiche);
-
-                if (!trouve)
-                {
                 btn.Background = Brushes.Red;
-                btn.IsEnabled = false;
                 vie--;
-            }
-                else
-                {
+                Lbl_Vies.Content = "Vies : " + vie;
 
-                btn.Background = Brushes.Green;
-                btn.IsEnabled = false;
+                if (vie <= 0)
                 {
-
-                
+                    MessageBox.Show("Perdu ! Le mot était : " + motMystere);
+                    InitialiserJeu();
                 }
+            }
+            else
+            {
+                btn.Background = Brushes.Green;
+
+                if (!motAffiche.Contains('_'))
+                {
+                    MessageBox.Show("Gagné ! Le mot était : " + motMystere);
+                    InitialiserJeu();
+                }
+            }
+        }
+
+        private void ReinitialiserBoutons()
+        {
+            foreach (var element in Grd_Keypad.Children)
+            {
+                if (element is Button btn)
+                {
+                    btn.IsEnabled = true;
+                    btn.Background = Brushes.LightGray;
                 }
             }
         }
     }
+}
